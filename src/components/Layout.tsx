9340ccd49a8806d2a3c51071396c85d16b9c9e1a
Sebastian Noriega <sebastian@usa.com>
2025-11-29 22:38:53 +0300

    Refactor opening flow to be inline in Case Detail

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,19 +5,21 @@ import { TopLiveBar } from './TopLiveBar';
 
 export const Layout: React.FC = () => {
   const location = useLocation();
-  // Hide top/bottom bars on specific pages if needed (e.g. full screen animation)
-  const isOpeningPage = location.pathname.includes('/opening');
-  // Also hide BottomNav on Case Detail Page (/cases/:id)
+  
+  // Check if we are on a case detail page
   const isCaseDetailPage = /^\/cases\/[^/]+$/.test(location.pathname);
-  const shouldHideBottomNav = isOpeningPage || isCaseDetailPage;
 
   return (
-    <div className="min-h-screen bg-[var(--tg-theme-bg-color)] text-[var(--tg-theme-text-color)] pb-24 pt-16 flex flex-col">
-      <TopLiveBar />
+    <div className="min-h-screen bg-[var(--tg-theme-bg-color)] text-[var(--tg-theme-text-color)] flex flex-col">
+      {/* Hide TopLiveBar on detail page for immersive experience */}
+      {!isCaseDetailPage && <TopLiveBar />}
+      
       <main className="w-full max-w-md mx-auto flex-1 flex flex-col min-h-0">
         <Outlet />
       </main>
-      {!shouldHideBottomNav && <BottomNav />}
+      
+      {/* Hide BottomNav on detail page */}
+      {!isCaseDetailPage && <BottomNav />}
     </div>
   );
 };
